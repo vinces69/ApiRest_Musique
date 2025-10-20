66358c0834e29ce69226b5e42169a4ba1b9584d9
Vinces C <vinces007@hotmail.com>
2025-10-20 18:48:39 +0200

    Ajout prise en compte des CORS

--- ApiRest_Musique/Program.cs
+++ ApiRest_Musique/Program.cs
@@ -4,22 +4,31 @@ using ApiRest_Musique_Bibliotheque.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<AlbumContext>();
-builder.Services.AddTransient<IAlbumRepository,AlbumRepository>();
+builder.Services.AddTransient<IAlbumRepository, AlbumRepository>();
 builder.Services.AddTransient<UtilisateurContext>();
 builder.Services.AddTransient<IUtilisateurRepository, UtilisateurRepository>();
 builder.Services.AddTransient<PossedeContext>();
 builder.Services.AddTransient<IPossedeRepository, PossedeRepository>();
 
+// ?? Ajout de la politique CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() )
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -29,6 +38,9 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+// ?? Activation de CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
